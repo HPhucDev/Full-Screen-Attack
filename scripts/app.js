874a8a639a5df39e0d7d3d74ad791070c9684ce9
HPhucDEV <hoangphucdeveloper@gmail.com>
2022-03-15 20:53:31 +0700

    Upload First

---
+++ scripts/app.js
@@ -0,0 +1,13 @@
+/*Dark mode */
+const switchElement = document.querySelector('.switch')
+switchElement.addEventListener('click', () => {
+  document.body.classList.toggle('dark')
+})
+const switchElement_mobile = document.querySelector('.switch__mobile')
+switchElement_mobile.addEventListener('click', () => {
+  document.body.classList.toggle('dark')
+})
+/*Dark mode */
+
+
+
